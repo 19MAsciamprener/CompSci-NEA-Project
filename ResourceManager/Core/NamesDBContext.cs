2d7a5344fea4ce2bbd625adc9b794f4ecc2dd71d
19MAsciamprener <19masciamprener@ashfieldstudent.ttct.co.uk>
2024-11-18 12:50:58 +0000

    Testing SQLite

--- ResourceManager/Core/NamesDBContext.cs
+++ ResourceManager/Core/NamesDBContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace ResourceManager.Core
 {
-    internal class NamesDBContext
+    internal class NamesDBContext : DbContext
     {
+        public DbSet<Names> names { get: set: }
+
+        public string path = @"C:\Temp\Names.db";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={path}");
     }
 }
